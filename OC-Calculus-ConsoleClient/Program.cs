46e123c2ea5b4aac1bcfeea8d8a199c25482c5c8
Housni BOUDAOUD <h.boudaoud@hotmail.fr>
2020-01-09 13:25:05 +0100

    Solution de l'exercice

---
+++ OC-Calculus-ConsoleClient/Program.cs
@@ -0,0 +1,105 @@
+﻿using OC_Calculus_ConsoleClient.OperationsArithmetiquesService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OC_Calculus_ConsoleClient
+{
+    class Program
+    {
+        public static double Infinity { get; private set; }
+        public static double NaN { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            // var clientOperationsArithmetiquesService = new ();
+            OperationsArithmetiquesClient client = new OperationsArithmetiquesClient();
+
+            //Les operations +, -, * et /
+            Console.WriteLine(" ------------ Les operations +, , * et / ------------ ");
+
+            Console.WriteLine($"42.514 + 13.967 = {client.Plus(42.514d, 13.967d)} \ndoit renvoyer 56.481");
+
+            Console.WriteLine($"42.514 + 13.967 = {client.Moins(42.514d, 13.967d)} \ndoit renvoyer 28.547");
+
+            Console.WriteLine($"4.5 * 11.5 = {client.Multiplier(4.5d, 11.5d)} \ndoit renvoyer 51.75");
+
+            string infinity = "Infinity";
+            Console.WriteLine($"51.75/11.5 = {client.Diviser(51.75d, 11.5d)} \ndoit renvoyer 4.5");
+            Console.WriteLine(
+                $"51.75/0 = " +
+                $"{((client.Diviser(51.75d, 0) == client.Diviser(51.75d, 0) + 100) ? infinity : client.Diviser(51.75d, 0).ToString())} " +
+                $"//doit renvoyer {infinity} "
+                );
+
+            //Est Multiple De
+            Console.WriteLine("\n ------------ Est Multiple De ------------ ");
+
+            Console.WriteLine($"45 est multiple de 5 : {client.EstMultipleDe(45, 5)} \ndoit renvoyer true");
+
+            Console.WriteLine($"60 est multiple de 13 : {client.EstMultipleDe(60, 13)} \ndoit renvoyer false");
+            //Additions Entieres
+            Console.WriteLine("\n ------------ Additions Entieres ------------ ");
+            String chaine = "68 + 14 + 7 + 29";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEntieres(chaine)} \ndoit renvoyer 118");
+            chaine = "48";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEntieres(chaine)} \ndoit renvoyer 48");
+            chaine = "";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEntieres(chaine)} \ndoit renvoyer 0");
+
+            //Additions Reelles
+            Console.WriteLine("\n ------------ Additions Reelles ------------ ");
+            chaine = "68,48+14,21+7,8+2,63";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsReelles(chaine)} \ndoit renvoyer 93,12");
+            chaine = "48,12";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsReelles(chaine)} \ndoit renvoyer 48.12");
+            chaine = "48.12";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsReelles(chaine)} \ndoit renvoyer 48.12");
+            chaine = "";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsReelles(chaine)} \ndoit renvoyer 0");
+
+
+
+            //Additions Et De Soustractions Entieres
+            Console.WriteLine("\n ------------ Additions Et De Soustractions Entieres ------------ ");
+            chaine = "-18+65-14+78";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsEntieres(chaine)} \ndoit renvoyer 111");
+            chaine = "208+66+14";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsEntieres(chaine)} \ndoit renvoyer 288");
+            chaine = "";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsEntieres(chaine)} \ndoit renvoyer 0");
+
+
+
+            //Additions Et De Soustractions Reelles
+            Console.WriteLine("\n ------------ Additions Et De Soustractions Entieres ------------ ");
+            chaine = "-18,48+65,44-14,484+78,221";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsReelles(chaine)} \ndoit renvoyer 110,697");
+            chaine = "208.48315184";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsReelles(chaine)} \ndoit renvoyer 208,48315184");
+            chaine = "208,48315184";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsReelles(chaine)} \ndoit renvoyer 208,48315184");
+            chaine = "";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtDeSoustractionsReelles(chaine)} \ndoit renvoyer 0");
+
+
+
+
+            //Additions Et Soustractions Et Multiplications Entieres
+            Console.WriteLine("\n ------------ Additions Et Soustractions Et Multiplications Entieres ------------ ");
+            chaine = "-69*18+14-22*-75";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtSoustractionsEtMultiplicationsEntieres(chaine)} \ndoit renvoyer 422");
+            chaine = "208*66*14-48*52";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtSoustractionsEtMultiplicationsEntieres(chaine)} \ndoit renvoyer 189696");
+            chaine = "";
+            Console.WriteLine($" {chaine} = {client.ChaineDAdditionsEtSoustractionsEtMultiplicationsEntieres(chaine)} \ndoit renvoyer 0");
+
+            client.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OC-Calculus-WCFService/OperationsArithmetiques.svc.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OC_Calculus_WCFService
+{
+    public class OperationsArithmetiques : IOperationsArithmetiques
+    {
+        public double Plus(double nb1, double nb2)
+        {
+            //TODO
+            return nb1 + nb2;
+        }
+
+        public double Moins(double nb1, double nb2)
+        {
+            //TODO
+            return nb1 - nb2;
+        }
+
+        public double Diviser(double nb1, double nb2)
+        {
+            //TODO
+            return nb1 / nb2;
+        }
+
+        public double Multiplier(double nb1, double nb2)
+        {
+            //TODO
+            return nb1 * nb2;
+        }
+
+        public bool EstMultipleDe(int nb1, int nb2)
+        {
+            //TODO
+            return (nb1 % nb2 == 0);
+        }
+
+        public int ChaineDAdditionsEntieres(string chaine)
+        {
+            //TODO
+
+            return Convert.ToInt32(ChaineDAdditionsReelles(chaine));
+        }
+
+        public double ChaineDAdditionsReelles(string chaine)
+        {
+            //TODO
+            double sum = 0;
+            if (chaine != null && chaine != "")
+            {
+                //double r1 = Convert.ToDouble("3.2");
+                double r = Convert.ToDouble("3,2");
+                chaine = chaine.Replace('.', ',');
+                List<string> chaine_nb = chaine.Split('+').ToList();
+                foreach (string item in chaine_nb)
+                {
+                    double value = (item=="") ?0:Convert.ToDouble(item);
+                    sum += value;
+                }
+            }
+
+            //TODO
+            return sum;
+        }
+        public int ChaineDAdditionsEtDeSoustractionsEntieres(string chaine)
+        {
+            //TODO
+
+            return Convert.ToInt32(ChaineDAdditionsEtDeSoustractionsReelles(chaine));
+        }
+        public double ChaineDAdditionsEtDeSoustractionsReelles(string chaine)
+        {
+            //TODO
+            if (chaine != null && chaine != "")
+            {
+
+                chaine = chaine.Replace(".", ",").Replace("-+", "-").Replace("+-", "-").Replace("--", "+");
+                chaine = chaine.Replace("-", "+-");
+                return ChaineDAdditionsReelles(chaine);
+
+                //// Une autre methode
+
+                //List<string> list_ChaineAddition = ("0" + chaine).Split('-').ToList();
+                //double resultat = ChaineDAdditionsReelles(list_ChaineAddition[0]);
+                //list_ChaineAddition.Remove(list_ChaineAddition[0]);
+                //foreach (string item in list_ChaineAddition)
+                //{
+                //    resultat += ChaineDAdditionsReelles("-" + item);
+                //}
+
+                //return resultat;
+            }
+            return 0;
+        }
+        public int ChaineDAdditionsEtSoustractionsEtMultiplicationsEntieres(string chaine)
+        {
+            //TODO
+            int resultat = 0;
+            if (chaine != null && chaine != "")
+            {
+
+                chaine = chaine.Replace("-+", "-").Replace("+-", "-").Replace("--", "+").Replace("*-", "¤");
+                chaine = chaine.Replace("-", "+-");
+                //list_ChaineSoustracion.Remove(list_ChaineSoustracion[0]);
+                List<string> list_ChaineAddition = chaine.Split('+').ToList();
+                foreach (string itemAddition in list_ChaineAddition)
+                {
+                    string item = (itemAddition.Contains("¤")) ? itemAddition.Replace("¤", "*-") : itemAddition;
+
+                    if (item.Contains("*") )
+                    {
+                        List<string> list_ChaineProduit = item.Split('*').ToList();
+                        int produit = 1;
+                        foreach (string itemProduit in list_ChaineProduit)
+                        {
+                            produit *= Convert.ToInt32(itemProduit);
+                        }
+
+                        resultat += produit;
+                    }
+                    else
+                    {
+                        resultat += ((item == "") ? 0 : Convert.ToInt32(item));
+                    }
+                }
+            }
+            return resultat;
+        }
+    }
+}
